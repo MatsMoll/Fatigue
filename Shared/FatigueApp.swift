655ca750353569abb860a47944db2914dc3c2f2f
Mats Eikeland Mollestad <mem@mollestad.no>
2021-05-29 18:57:48 +0200

    Initial Commit

---
+++ Shared/FatigueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FatigueApp.swift
+//  Shared
+//
+//  Created by Mats Mollestad on 29/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FatigueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
